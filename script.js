86e40dfaf3689d8adcecea3d54a04a0a796f8ea7
Parisa Esmaeilpour <esmaeilpour.parisa@gmail.com>
2025-03-29 10:35:29 +0330

    complete game.

---
+++ script.js
@@ -0,0 +1,77 @@
+const numberInput = document.getElementById("guess-number");
+const correct = document.getElementById("correct");
+const higher = document.getElementById("higher");
+const lower = document.getElementById("lower");
+const owlImg = document.getElementById("owl");
+const leftBar = document.getElementById("left-bar");
+const rightBar = document.getElementById("right-bar");
+const chanceRemain = document.getElementById("chance-remain");
+const higherNo = document.getElementById("higher-no");
+const lowerNo = document.getElementById("lower-no");
+const errorHandling = document.getElementById("error-handling")
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+let guessNumber = 10;
+
+maxLower = null;
+minHigher = null
+
+numberInput.addEventListener("keyup", (event) => {
+  if (event.code === "Enter") {
+    errorHandling.classList.add("error-handling")
+    const userInput = event.target.value.trim();
+    if(userInput==="" || isNaN(userInput)){
+      errorHandling.classList.remove("error-handling")
+      event.target.value = ""
+      return
+    }
+
+    const userGuess = +event.target.value;
+
+    if (guessNumber === 1) {
+      leftBar.innerHTML = `
+      <h1 class="result-header" id="result-header">You Lost!</h1>
+      `;
+      owlImg.src = "./assets/imgs/Picsart_24-11-22_14-41-40-977.png";
+      rightBar.style.display = "none";
+    }
+
+    if (randomNumber === userGuess) {
+      correct.classList.add("fire-color");
+      higher.classList.remove("fire-color");
+      lower.classList.remove("fire-color");
+
+      setTimeout(() => {
+        leftBar.innerHTML = `
+        <h1 class="result-header" id="result-header">You Won!!!</h1>
+              <span class="result-text">The Correct Number Is: ${userGuess}</span>
+        `;
+        owlImg.src = "./assets/imgs/happy.png";
+        rightBar.style.display = "none";
+      }, 1000);
+    }
+    if (randomNumber > userGuess) {
+      lower.classList.add("fire-color");
+      higher.classList.remove("fire-color");
+      
+      if(maxLower === null || userGuess > maxLower){
+        maxLower = userGuess;
+      }
+      lowerNo.innerHTML = maxLower;
+    }
+
+    if (randomNumber < userGuess) {
+      higher.classList.add("fire-color");
+      lower.classList.remove("fire-color");
+
+      if(minHigher === null || userGuess < minHigher){
+        minHigher = userGuess;
+      }
+      higherNo.innerHTML = minHigher;
+    }
+    guessNumber--;
+    chanceRemain.innerHTML = guessNumber;
+    event.target.value=""
+  }
+});
